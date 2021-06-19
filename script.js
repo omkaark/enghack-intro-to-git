f53439f193cb0c7eaa62c55cfe5450fa0bee4097
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-06-19 16:06:54 -0400

    Fix js bugs

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 // Function to display value
 function displayAnswer(val) {
-    document.getElementById("result").value += val;
+  document.getElementById("result").value += val;
 }
 
 //function that evaluates the digit and return result
 function solveEquation() {
-    let x = document.getElementById("result").value;
-    let y = eval(x); // Built-in javascript funtion that calculates a result from a string
-    document.getElementById("result").value = y;
+  let x = document.getElementById("result").value;
+  let y = eval(x); // Built-in javascript funtion that calculates a result from a string
+  document.getElementById("result").value = y;
 }
 
 //function that clear the display
 function clearText() {
-    document.getElementById("result").value = "What?"; // Mistake: Value given should be ""
+  document.getElementById("result").value = ""; // Mistake: Value given should be ""
 }
